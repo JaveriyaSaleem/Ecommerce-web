7d8624a7f200df5e08ab03586a9cda81778e690d
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-05-13 13:44:57 +0500

    working good some functionalities left to be added

--- Frontend/src/Components/DashboardComp.jsx/TopSelling/Products.jsx
+++ Frontend/src/Components/DashboardComp.jsx/TopSelling/Products.jsx
@@ -4,17 +4,7 @@ import Swal from 'sweetalert2';
 
 const Products = (props) => {
   const navigate = useNavigate();
-    const CartFunction = () => {
-    if (!localStorage.getItem("token")) {
-      Swal.fire({
-          text: "Please Login to Add the Product in the Cart",
-        });
-      navigate("/login")}
-      else{
-        console.log("Product added to cart");
-      }
-
-    }
+    
   return (
     <div className='flex flex-col gap-3 mb-10'>
       <figure className='overflow-hidden'><img src={props.img} alt="img of a shirt" className='transition duration-300 ease-in-out hover:scale-[1.04] rounded-2xl' /></figure>
@@ -22,7 +12,7 @@ const Products = (props) => {
         
         <h3 className='font-bold' >{props.name}</h3>
         <h3>{props.price}</h3>
-        <div><button className='cursor-pointer border py-2 px-4 rounded-full bg-black text-white hover:scale-[1.07] transition duration-300 ease-in-out' onClick={CartFunction}>Add to Cart</button> <button className='cursor-pointer border py-2 px-4 rounded-full hover:scale-[1.07] transition duration-300 ease-in-out' onClick={props.onClick}>View Item</button>
+        <div><button className='cursor-pointer border py-2 px-4 rounded-full bg-black text-white hover:scale-[1.07] transition duration-300 ease-in-out' onClick={props.functionForCart}>Add to Cart</button> <button className='cursor-pointer border py-2 px-4 rounded-full hover:scale-[1.07] transition duration-300 ease-in-out' onClick={props.onClick}>View Item</button>
         </div>
       </div>
     </div>
--- Frontend/src/Components/ViewAllShopComp/AllProductsShop.jsx
+++ Frontend/src/Components/ViewAllShopComp/AllProductsShop.jsx
@@ -8,6 +8,8 @@ import { useContext } from 'react';
 import { MyContext } from "../../Context/MyContext.jsx";
 import ErrorBoundary from '../../ErrorBoundary.jsx';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast,Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const AllProductsShop = () => {
   const navigate = useNavigate();
   const { setProductId,setapi } = useContext(MyContext); //context to get the productId
@@ -21,9 +23,33 @@ const AllProductsShop = () => {
         setTimeout(() => {
           navigate('/product')
         }, 2000);
-        
-        
       }
+      const CartFunction =async (myproductId) => {
+    if (!localStorage.getItem("token")) {
+      Swal.fire({
+          text: "Please Login to Add the Product in the Cart",
+        });
+      navigate("/login")}
+      else{
+        const response = await axios.put(`http://localhost:3000/cart`, {
+      productId: myproductId,
+      userId: localStorage.getItem("token"),
+    });
+    console.log(response.data)
+toast.success('Product added to cart!', {
+position: "top-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "colored",
+transition: Bounce,
+});
+      }
+
+    }
    const filterFunction = (e) => {
 console.log(e.target.innerText)
 setFilter(e.target.innerText)
@@ -51,44 +77,59 @@ setAllProducts(response.data)
   }, []);
 
   return (
-    <ErrorBoundary>
-      <div className='px-3'>
-        <Navbar/>
-        <div className=' pb-6'>
-        <p className='flex items-center fixed'>
-        <span className='text-[#888383]'>Home</span> <IoIosArrowForward/> Shop All</p>
-        </div>
-        <main className='grid grid-cols-12 gap-10 pt-5'>
-          {/* left side  */}
-          <div className='col-span-2'>
-          <div className='border  rounded-2xl  h-100 fixed w-40 px-2 py-4'>
-            <h3 className='flex items-center justify-between border-b pb-4'><span className=''>Filters</span> <HiOutlineAdjustments className='text-[#aeaeae] text-[20px]'/></h3>
-            <div className='flex flex-col text-[#aeaeae] pt-4'>
-              {/* t-shirt  */}
-            <div  onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>t-shirts </button><IoIosArrowForward/></div>
-            {/* shirts  */}
-            <div onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>shirts </button><IoIosArrowForward/></div>
-            {/* jeans  */}
-            <div onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>jeans </button><IoIosArrowForward/></div>
-            {/* shorts  */}
-            <div onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>shorts</button><IoIosArrowForward/></div>
-            </div>
-            </div>
-            </div>
-            {/* right side  */}
-      
-          <div className='col-span-8'>
-            <h1 className='text-[32px] font-bold'></h1>
-      <div className='grid grid-cols-3 gap-3'>
-        {products.map((product,id) => (
-          <Products key={id} img={product.Image} name={product.ProductName
-      } price={`$${product.Price}`} onClick={()=>viewFunction(product._id)}/>
-        ))}
-      </div>
+    <div>
+      <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="colored"
+transition={Bounce}
+/>
+      <ErrorBoundary>
+        <div className='px-3'>
+          <Navbar/>
+          <div className=' pb-6'>
+          <p className='flex items-center fixed'>
+          <span className='text-[#888383]'>Home</span> <IoIosArrowForward/> Shop All</p>
           </div>
-        </main>
-      </div>
-    </ErrorBoundary>
+          <main className='grid grid-cols-12 gap-10 pt-5'>
+            {/* left side  */}
+            <div className='col-span-2'>
+            <div className='border  rounded-2xl  h-100 fixed w-40 px-2 py-4'>
+              <h3 className='flex items-center justify-between border-b pb-4'><span className=''>Filters</span> <HiOutlineAdjustments className='text-[#aeaeae] text-[20px]'/></h3>
+              <div className='flex flex-col text-[#aeaeae] pt-4'>
+                {/* t-shirt  */}
+              <div  onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>t-shirts </button><IoIosArrowForward/></div>
+              {/* shirts  */}
+              <div onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>shirts </button><IoIosArrowForward/></div>
+              {/* jeans  */}
+              <div onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>jeans </button><IoIosArrowForward/></div>
+              {/* shorts  */}
+              <div onClick={filterFunction}  className='flex items-center justify-between cursor-pointer hover:text-black transition'><button>shorts</button><IoIosArrowForward/></div>
+              </div>
+              </div>
+              </div>
+              {/* right side  */}
+      
+            <div className='col-span-8'>
+              <h1 className='text-[32px] font-bold'></h1>
+        <div className='grid grid-cols-3 gap-3'>
+          {products.map((product,id) => (
+            <Products key={id} img={product.Image} name={product.ProductName
+        } price={`$${product.Price}`} functionForCart={()=>CartFunction(product._id)} onClick={()=>viewFunction(product._id)}/>
+          ))}
+        </div>
+            </div>
+          </main>
+        </div>
+      </ErrorBoundary>
+    </div>
   )
 }
 
