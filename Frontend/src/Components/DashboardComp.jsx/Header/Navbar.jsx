ab9f3a4dfd5a36f467f15f6b4762d0653e83f7a0
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-05-29 12:58:52 +0500

    added

--- Frontend/src/Components/DashboardComp.jsx/Header/Navbar.jsx
+++ Frontend/src/Components/DashboardComp.jsx/Header/Navbar.jsx
@@ -5,45 +5,46 @@ import { Link, useNavigate } from "react-router-dom";
 import CartIcon from "./CartIcon";
 import axios from "axios";
 import { useContext } from "react";
-import { MyContext } from "../../../Context/MyContext.jsx"
+import { MyContext } from "../../../Context/MyContext.jsx";
 import DropdownMenu from "./Dropdown.jsx";
 const Navbar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
-  const [cartIconItems,setCartIconItems]= useState([])
-  const {cartItems} = useContext(MyContext);
+  const [cartIconItems, setCartIconItems] = useState([]);
+  const { cartItems } = useContext(MyContext);
   const [showLogin, setShowLogin] = useState(false);
-  const cart=async()=>{
-    try{
+  const cart = async () => {
+    try {
       const response = await axios.get(`http://localhost:3000/cart`);
       // const cart = response.data;
-      const filterCart = response.data.filter((item)=>item.userId===localStorage.getItem("token"))
-      console.log("data found",filterCart);
+      const filterCart = response.data.filter(
+        (item) => item.userId === localStorage.getItem("token")
+      );
+      console.log("data found", filterCart);
       setCartIconItems(filterCart);
-    }catch(e){
-      console.log(e)
+    } catch (e) {
+      console.log(e);
     }
-  }
-  useEffect(() => { 
-    cart()
-if(localStorage.getItem("token")){
-  setShowLogin(true)}
-  
-  else{
-    setShowLogin(false)
-  }},[])
+  };
+  useEffect(() => {
+    cart();
+    if (localStorage.getItem("token")) {
+      setShowLogin(true);
+    } else {
+      setShowLogin(false);
+    }
+  }, []);
 
-useEffect(() => {
-cart()
-}, [cartItems])
-  
+  useEffect(() => {
+    cart();
+  }, [cartItems]);
+
   return (
     <nav className="px-4">
-      <div className="grid grid-cols-5 w-full py-4">
+      <div className="flex justify-between items-center md:grid md:grid-cols-5 w-full py-4">
         {/* Logo */}
         <Link to="/">
-          
-          <h1 className='text-[32px] font-extrabold boldFont w-40'>SHOP.CO</h1>
+          <h1 className="text-[32px] font-extrabold boldFont w-40">SHOP.CO</h1>
         </Link>
 
         {/* Hamburger menu for mobile */}
@@ -72,25 +73,46 @@ cart()
         {/* Desktop Nav */}
         <div className="hidden md:flex items-center justify-between  col-span-4">
           <div className="flex justify-center gap-10">
-          <Link to="/shopAll"><a className="">Shop</a></Link>
-          <Link to="/shopAll"><a className=" hover: hover:font-bold">On Sale</a></Link>
-          <Link to="/shopAll"><a className=" hover: hover:font-bold">New Arrivals</a></Link>
-          <Link to="/shopAll"><a className=" hover: hover:font-bold">Brands</a></Link>
+            <Link to="/shopAll">
+              <a className="">Shop</a>
+            </Link>
+            <Link to="/shopAll">
+              <a className=" hover: hover:font-bold">On Sale</a>
+            </Link>
+            <Link to="/shopAll">
+              <a className=" hover: hover:font-bold">New Arrivals</a>
+            </Link>
+            <Link to="/shopAll">
+              <a className=" hover: hover:font-bold">Brands</a>
+            </Link>
           </div>
 
-           <div className='hidden border rounded-full w-80 items-center justify-items-start py-2 px-2'><IoIosSearch className=''/> <input type="text" placeholder='Search for products..' className='ps-2 outline-0'/></div>
-                  <div className='flex justify-between font-bold items-center gap-3 '>
-<CartIcon itemCount={cartIconItems[0]?.products?.length || 0} />
+          <div className="hidden border rounded-full w-80 items-center justify-items-start py-2 px-2">
+            <IoIosSearch className="" />{" "}
+            <input
+              type="text"
+              placeholder="Search for products.."
+              className="ps-2 outline-0"
+            />
+          </div>
+          <div className="flex justify-between font-bold items-center gap-3 ">
+            <CartIcon itemCount={cartIconItems[0]?.products?.length || 0} />
 
-                  
-                  
-                  <div>
-                  {showLogin? 
-  <DropdownMenu/>
-                  :<button className="font-light border px-3 py-1 rounded-full hover:cursor-pointer hover:text-white hover:bg-black transition  text-[14px]" onClick={()=>{navigate('/login')}}>Login/Register</button>}
-                  </div>
-                 
-                  </div>
+            <div>
+              {showLogin ? (
+                <DropdownMenu />
+              ) : (
+                <button
+                  className="font-light border px-3 py-1 rounded-full hover:cursor-pointer hover:text-white hover:bg-black transition  text-[14px]"
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  Login/Register
+                </button>
+              )}
+            </div>
+          </div>
         </div>
       </div>
 
@@ -98,15 +120,50 @@ cart()
       {isOpen && (
         <div className="flex justify-center pb-10 absolute top-20 left-0 w-full bg-white shadow-md space-y-10 py-6 md:hidden">
           <div className="flex flex-col items-center gap-4">
-          <a href="#" className=" font-bold">Home</a>
-          <a href="#" className=" hover:font-bold">Portfolio</a>
-          <a href="#" className=" hover:font-bold">About Me</a>
-          <a href="#" className=" hover:font-bold">Testimonials</a>
-          <div>
-          </div>
-          <button className="border border-[#5e3bee] text-[#5e3bee] hover:text-white hover:bg-[#5e3bee] py-2 px-3 rounded-sm">
-            Contact Me
-          </button>
+            <Link to="/">
+              <a className=" font-bold">
+                Home
+              </a>
+            </Link>
+            <Link to="/shopAll">
+              <a href="" className=" hover:font-bold">
+                Shop
+              </a>
+            </Link>
+            {showLogin?(<Link to="/profile">
+              <a  className=" hover:font-bold">
+                Profile
+              </a>
+            </Link>):navigate('/login')}
+            
+
+            {/* <a href="#" className=" hover:font-bold"></a> */}
+            <div></div>
+            <div>
+              {showLogin ? (
+                
+                <button
+                  className="font-light border px-3 py-1 rounded-full hover:cursor-pointer hover:text-white hover:bg-black transition  text-[14px]"
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    setTimeout(() => {
+                      navigate("/login");
+                    }, 1000);
+                  }}
+                >
+                  Logout
+                </button>
+              ) : (
+                <button
+                  className="font-light border px-3 py-1 rounded-full hover:cursor-pointer hover:text-white hover:bg-black transition  text-[14px]"
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  Login/Register
+                </button>
+              )}
+            </div>
           </div>
         </div>
       )}
