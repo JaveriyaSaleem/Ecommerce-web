60d8d473a31e49bb66ebaaf03a5d9b8216460989
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-07-31 00:26:34 +0500

    added

--- Frontend/src/Components/DashboardComp.jsx/Footer.jsx
+++ Frontend/src/Components/DashboardComp.jsx/Footer.jsx
@@ -1,23 +1,69 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { FaTwitter,FaFacebookF, FaInstagram  } from "react-icons/fa";
+import { FaTwitter,FaFacebookF, FaInstagram,FaGithub   } from "react-icons/fa";
+import {paymentMethods} from '../../assets/Images/pay.png'
 
 
 const Footer = () => {
   return (
-    <div className='bg-[#f0f0f0] py-20 pb-10 relative '>
-       {/* Logo */}
-        <Link to='/'>
-          <h1 className='text-[32px] font-extrabold boldFont md:w-40 text-center'>SHOP.CO</h1>
-        </Link>
-       <div className="flex flex-col md:flex-row justify-between py-3 text-center md:text-left">
-        <p className='md:w-lg'>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
-        <div className='flex  justify-around md:justify-between pt-10 md:p-0 md:gap-3'><FaTwitter className='transition duration-150 ease-in-out text-[35px] hover:scale-[1.07] py-2 px-1 border rounded-full bg-black text-white'/> <FaFacebookF className='transition duration-duration-150 ease-in-out text-[35px] hover:scale-[1.07] py-2 px-1 border rounded-full bg-black text-white'/> <FaInstagram className='transition duration-duration-150 ease-in-out text-[35px] hover:scale-[1.07] py-2 px-1 border rounded-full bg-black text-white'/></div> 
-        </div>
-        <div className='text-center'>
-         <p>Made with 💖 by <a href='https://github.com/JaveriyaSaleem' className='font-bold'>@JaveriyaSaleem </a></p>
-        </div>
-    </div>
+    <main className='bg-[#f0f0f0] pb-10 pt-10'>
+      <div className='py-20 pb-10 relative flex justify-around border-b border-[#cecbcb] mx-10'>
+         {/* Logo */}
+          <main className='flex flex-col justify-around '>
+            <Link to='/'>
+              <h1 className='text-[32px] font-extrabold boldFont md:w-40 text-center'>SHOP.CO</h1>
+            </Link>
+            <div className="flex flex-col md:flex-row justify-between py-3 text-center md:text-left">
+            <p className='md:w-lg'>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
+            </div>
+                      <div className='flex justify-start md:justify-start pt-10 md:p-0 md:gap-3'><FaTwitter className='transition duration-150 ease-in-out text-[35px] hover:scale-[1.07] py-2 px-1 border rounded-full bg-black text-white'/> <FaFacebookF className='transition duration-duration-150 ease-in-out text-[35px] hover:scale-[1.07] py-2 px-1 border rounded-full bg-black text-white'/> <FaInstagram className='transition duration-duration-150 ease-in-out text-[35px] hover:scale-[1.07] py-2 px-1 border rounded-full bg-black text-white'/>
+                      <FaGithub className='transition duration-duration-150 ease-in-out text-[35px] hover:scale-[1.07] py-2 px-1 border rounded-full bg-black text-white'/></div>
+          </main>
+      {/* right side  */}
+          <main className='grid grid-cols-4 gap-8'>
+            <div className='text-base/8'>
+              <h3 className='font-bold pb-2'>COMPANY</h3>
+              <ul>
+                <li>About</li>
+                <li>Features</li>
+                <li>Works</li>
+                <li>Career</li>
+              </ul>
+            </div>
+              <div className='text-base/8'>
+              <h3 className='font-bold pb-2'>HELP</h3>
+              <ul>
+                <li>Customer Support</li>
+                <li>Delivery Details</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </div>
+            <div className='text-base/8'>
+              <h3 className='font-bold pb-2'>FAQ</h3>
+              <ul>
+                <li>Account</li>
+                <li>Manage Deliveries</li>
+                <li>Orders</li>
+                <li>Payments</li>
+              </ul>
+            </div>
+            <div className='text-base/8'>
+              <h3 className='font-bold pb-2'>RESOURCES</h3>
+              <ul>
+                <li>Free eBooks</li>
+                <li>Development Tutorial</li>
+                <li>How to Blog</li>
+                <li>Youtube Playlist</li>
+              </ul>
+            </div>
+          </main>
+      </div>
+      <div className='px-12 pt-6'>
+        <p>Made with 💖 by <a href="https://github.com/JaveriyaSaleem" className='font-bold'>Javeriya</a>. All Right Reserved</p>
+        <figure><img src={paymentMethods} alt="img" /></figure>
+      </div>
+    </main>
   )
 }
 
--- Frontend/src/Components/DashboardComp.jsx/TopSelling/Products.jsx
+++ Frontend/src/Components/DashboardComp.jsx/TopSelling/Products.jsx
@@ -7,11 +7,11 @@ const Products = (props) => {
   const navigate = useNavigate();
     
   return (
-    <div className='flex flex-col gap-3 mb-10 w-70'>
-      <figure className='overflow-hidden rounded-2xl'><img src={props.img} alt="img of a shirt" className='w-70 object-contain p-4 transition duration-300 ease-in-out hover:scale-[1.04] rounded-2xl' /></figure>
+    <div className='flex flex-col gap-3 mb-10 w-70 rounded-2xl'>
+      <figure className='overflow-hidden'><img src={props.img} alt="img of a shirt" className='w-70 object-contain p-4 transition duration-300 ease-in-out hover:scale-[1.04] rounded-4xl' /></figure>
       <div className='flex flex-col gap-2 text-center'>
         
-        <h3 className='font-bold ' >{props.name}</h3>
+        <h3 className='font-bold'>{props.name}</h3>
         <h3 className="">{props.price}</h3>
         <div><button className='cursor-pointer border py-2 px-4 rounded-full bg-black text-white hover:scale-[1.07] transition duration-300 ease-in-out' onClick={props.functionForCart}>Add to Cart</button> <button className='cursor-pointer border py-2 px-4 rounded-full hover:scale-[1.07] transition duration-300 ease-in-out' onClick={props.onClick}>View Item</button>
         </div>
